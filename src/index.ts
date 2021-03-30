892d5fec9cfb02cec2e644a57a9f231f68b776a3
Mia Adjei <adjei.m@gmail.com>
2021-03-30 14:31:24 -0400

    Add CORS support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import roomsRouter from './routes/room';
 
 const app = express();
+
+// Add a list of allowed origins.
+// If you have more origins you would like to add, you can add them to the array below.
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+// Then pass these options to cors:
+app.use(cors(options));
 
 app.use(express.json());
 
