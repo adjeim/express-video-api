243704a9867e1d8bf7f7563d86fed87ba39f4b65
Mia Adjei <adjei.m@gmail.com>
2021-03-31 12:33:49 -0400

    Update to use API key + secret instead of auth token

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,8 @@ dotenv.config();
 
 const config = {
   TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN
+  TWILIO_API_KEY: process.env.TWILIO_API_KEY,
+  TWILIO_API_SECRET: process.env.TWILIO_API_SECRET
 };
 
 export default config;
